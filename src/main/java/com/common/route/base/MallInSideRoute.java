1989bd4730dce524824b49ccc383598cfae6feb6
jklofs <851996037@qq.com>
2018-05-31 16:47:59 +0800

    新增接口

--- src/main/java/com/common/route/base/MallInSideRoute.java
+++ src/main/java/com/common/route/base/MallInSideRoute.java
@@ -9,23 +9,25 @@ public interface MallInSideRoute {
      * @return
      */
     @RequestMapping(value = "/mall/v1/categories",method = RequestMethod.GET)
-    public String findCategory();
+    public String findCategory(@RequestHeader("appCode") String appCode);
 
     /**
      * 分类:子集类目
      *
+     * @param appCode
      * @return
      */
     @RequestMapping(value = "/mall/v1/subcategories",method = RequestMethod.GET)
-    public String findSubCategory(@RequestParam("categoryId") Integer categoryId);
+    public String findSubCategory(@RequestHeader("appCode") String appCode,@RequestParam("categoryId") Integer categoryId);
 
     /**
      * 商品详情
      *
+     * @param appCode
      * @param commodityIds
      * @return
      */
     @RequestMapping(value = "/mall/v1/commodities/message", method = RequestMethod.GET)
-    public String commoditiesMessage(@RequestParam(value = "commodityIds") String commodityIds);
+    public String commoditiesMessage(@RequestHeader("appCode") String appCode,@RequestParam(value = "commodityIds") String commodityIds);
 
 }
