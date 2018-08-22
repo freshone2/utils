6c2efe62a7b1f4bb8c09b17c8753a94b5b29d4a8
shuo xiang <xiangshuo@192.168.20.121>
2018-08-22 11:35:08 +0800

    获取skuId

---
+++ src/main/java/com/common/model/dto/commodity/CommoditySpecIdDTO.java
@@ -0,0 +1,34 @@
+package com.common.model.dto.commodity;
+
+public class CommoditySpecIdDTO {
+
+	private Integer specId;
+	private Integer commodityId;
+	private String specNo;
+	private String commodityNo;
+	
+	public Integer getSpecId() {
+		return specId;
+	}
+	public void setSpecId(Integer specId) {
+		this.specId = specId;
+	}
+	public Integer getCommodityId() {
+		return commodityId;
+	}
+	public void setCommodityId(Integer commodityId) {
+		this.commodityId = commodityId;
+	}
+	public String getSpecNo() {
+		return specNo;
+	}
+	public void setSpecNo(String specNo) {
+		this.specNo = specNo;
+	}
+	public String getCommodityNo() {
+		return commodityNo;
+	}
+	public void setCommodityNo(String commodityNo) {
+		this.commodityNo = commodityNo;
+	}
+}
